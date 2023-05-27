59d80140ec5b5d5f0a08260edec8b58f5d80969c
kevinVasquez <86323983+KeviNicolasVasquezCastro@users.noreply.github.com>
2023-05-27 14:06:40 -0500

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -2,14 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
-# configuración de Chrome para abrir en modo incógnito
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--incognito")
 
-# crea una instancia de WebDriver de Chrome con la configuración personalizada
-driver = webdriver.Chrome(chrome_options=chrome_options)
+driver = webdriver.Chrome()
 
 
 # maximizar la ventana de Chrome.
@@ -20,7 +18,7 @@ driver.get('https://my361486.crm.ondemand.com/sap/ap/ui/clogin?saml2=disabled&ap
 # ingresar usuario y contraseña.
 usuario = driver.find_element(By.XPATH, "//*[@id='USERNAME_FIELD-inner']").send_keys("kvasquez")
 
-contraseña = driver.find_element(By.XPATH, "//*[@id='PASSWORD_FIELD-inner']").send_keys("Colombia2026-")
+contraseña = driver.find_element(By.XPATH, "//*[@id='PASSWORD_FIELD-inner']").send_keys("Colombia2028-")
 
 # darle click al boton de iniciar sesion.
 
@@ -44,22 +42,22 @@ time.sleep(4)
 
 botonmas = driver.find_element(By.XPATH, "//*[@id='buttonMyhsQOk2saslFBU5NpnmTW_130-img']").click()
 
-time.sleep(6)
+time.sleep(7)
 
-# introducir cliente.
+# ingresar cliente.
 
-input_element = driver.find_element(By.XPATH, "//*[@id='objectvalueselectorI7pYpVvFLKch84EsXeFzk0_602-inputField']")
-span_element = input_element.find_element(By.TAG_NAME, "span").click()
+input_element = driver.find_element(By.XPATH, "//*[@id='objectvalueselectorI7pYpVvFLKch84EsXeFzk0_623-inputField-inner']")
+span_element = input_element.find_element(By.XPATH, "//*[@id='objectvalueselectorI7pYpVvFLKch84EsXeFzk0_623-inputField-vhi']").click()
 
-time.sleep(2)
+time.sleep(4)
 
-botonfiltro = driver.find_element(By.XPATH, "//*[@id='findformpaneFindFormPane_ID_683']").click()
+botonfiltro = driver.find_element(By.XPATH, "//*[@id='findformpaneFindFormPane_ID_709-inner']").click()
 
 time.sleep(2)
 
 codigounico = "024351041"
 
-cu = driver.find_element(By.XPATH, "//*[@id='inputfieldfacaf9767aaad205e608005c2cab9a21_709-inner']")
+cu = driver.find_element(By.XPATH, "//*[@id='inputfieldfacaf9767aaad205e608005c2cab9a21_735-inner']")
 
 cu.send_keys(codigounico)
 
@@ -67,22 +65,43 @@ cu.send_keys(Keys.ENTER)
 
 time.sleep(2)
 
-codigounico = driver.find_element(By.XPATH, "//*[@id='statictextooAT7P0BV46hbJtiB49eWG_660-listdefintionG3ws9fm8F4M0zWzfo_shp1G_654-0']").click()
+codigounico = driver.find_element(By.XPATH, "//*[@id='__item225-listdefintionG3ws9fm8F4M0zWzfo_shp1G_680-0']").click()
 
 time.sleep(3)
 
+# ingresar asunto.
+
+asunto = driver.find_element(By.XPATH, "//*[@id='inputfieldDJJuNyNvsqsbod89NjRCeW_624-inner']").send_keys("Ticket de prueba")
+
+time.sleep(2)
+
 # añadir producto.
-producto = driver.find_element(By.XPATH, "//*[@id='navigationitemnLj_abWLka6LtpHd3eUlqG_160']").click()
 
-time.sleep(1)
+producto = driver.find_element(By.XPATH, "//*[@id='objectvalueselectorKiq0_swnrw4A3HKbxwwRcvW_364-inputField-inner']")
 
-añadir = driver.find_element(By.XPATH, "//*[@id='buttonMJfG64VVQaE_2afTRvvUWG_784']").click()
+producto.send_keys("PASARELAS EXTERNAS")
 
-time.sleep(1)
+time.sleep(3)
 
-usuario = driver.find_element(By.XPATH, "//*[@id='objectvalueselectorne2Z4Bo6Lq25JpFbxu5XpW_852-inputField-inner']").send_keys("50100049")
+# ingresar terminal.
 
+terminalaingresar = "00004451"
 
+terminal = driver.find_element(By.XPATH, "//*[@id='inputfield9e99b202ed0583b774ac9a8bf282547c_368-inner']")
+
+terminal.send_keys(terminalaingresar)
+
+time.sleep(3)
+
+# Encontrar el elemento en el que deseas pegar el texto
+descripcion = driver.find_element(By.XPATH, "//*[@id='__area0']").click()
+
+time.sleep(3)
+
+# Encontrar el iframe
+iframe_xpath = "//*[@id='__area3-content'/table/tbody/tr[2]/td/iframe"
+iframe = driver.find_element(By.XPATH, iframe_xpath)
+iframe.send_keys("descripcion")
 
 time.sleep(10)
 
